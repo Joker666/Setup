fee6791b4c989d7bb1492e8649f567e183f33f56
Hasan <mr.k779@outlook.com>
2024-08-04 17:45:42 +0600

    Create .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,38 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'Batman'
+  else
+    return 'Papercolor Light (Gogh)'
+  end
+end
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = scheme_for_appearance(get_appearance())
+config.font = wezterm.font('JetBrains Mono', { weight = 'Bold' })
+config.font_size = 14.0
+config.window_close_confirmation = 'NeverPrompt'
+config.default_cursor_style = 'BlinkingBar'
+config.initial_cols = 100
+
+-- and finally, return the configuration to wezterm
+return config
